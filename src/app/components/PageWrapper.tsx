d9aa8aea135da12810f957a9fac80f15b9a6f59c
andrewgremlich <andrew.gremlich@gmail.com>
2025-12-29 19:16:24 -0700

    full height bug

--- src/app/components/PageWrapper.tsx
+++ src/app/components/PageWrapper.tsx
@@ -1,6 +1,6 @@
 export const PageWrapper = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="bg-background text-foreground">
+		<div className="bg-background text-foreground h-full min-h-screen">
 			<div className="container mx-auto py-25">{children}</div>
 		</div>
 	);
