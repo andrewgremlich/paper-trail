ac42689931373abbc16e112edfe45a700e697de3
andrewgremlich <andrew.gremlich@gmail.com>
2026-01-22 15:49:43 -0700

    updating add transaction form

--- src/app/components/features/transactions/TransactionViewRow/index.tsx
+++ src/app/components/features/transactions/TransactionViewRow/index.tsx
@@ -7,78 +7,90 @@ import { formatDate } from "@/lib/utils";
 import styles from "./styles.module.css";
 
 interface TransactionViewRowProps {
-	tx: Transaction;
-	projects: Project[] | undefined;
-	path: string;
-	onEdit: () => void;
-	onDelete: (formData: FormData) => Promise<void>;
+  tx: Transaction;
+  projects: Project[] | undefined;
+  path: string;
+  onEdit: () => void;
+  onDelete: (formData: FormData) => Promise<void>;
 }
 
 export const TransactionViewRow = ({
-	tx,
-	projects,
-	path,
-	onEdit,
-	onDelete,
+  tx,
+  projects,
+  path,
+  onEdit,
+  onDelete,
 }: TransactionViewRowProps) => (
-	<>
-		<TD>
-			<span>{formatDate(tx.date)}</span>
-		</TD>
-		<TD>
-			<span>{tx.description}</span>
-		</TD>
-		<TD>
-			<span>
-				{projects?.find((project) => project.id === tx.projectId)?.name}
-			</span>
-		</TD>
-		<TD>
-			<span>${tx.amount.toFixed(2)}</span>
-		</TD>
-		<TD>
-			{path.length > 0 ? (
-				<Button
-					type="button"
-					size="sm"
-					variant="ghost"
-					onClick={async () => {
-						await openAttachment(path);
-					}}
-				>
-					{path.startsWith("http://") || path.startsWith("https://") ? (
-						<Globe color="black" />
-					) : (
-						<FolderOpen color="black" />
-					)}
-				</Button>
-			) : (
-				<span className={styles.noFile}>No File</span>
-			)}
-		</TD>
-		<TD>
-			<Button variant="ghost" type="button" onClick={onEdit}>
-				<Edit color="black" />
-			</Button>
-		</TD>
-		<TD>
-			<form
-				onSubmit={async (evt) => {
-					evt.preventDefault();
-					const fd = new FormData(evt.currentTarget);
-					await onDelete(fd);
-				}}
-			>
-				<input type="hidden" name="id" value={tx.id} />
-				<Button
-					variant="ghost"
-					size="icon"
-					type="submit"
-					aria-label="Delete Transaction"
-				>
-					<TrashIcon color="black" />
-				</Button>
-			</form>
-		</TD>
-	</>
+  <>
+    <TD>
+      <span>{formatDate(tx.date)}</span>
+    </TD>
+    <TD>
+      <span>{tx.description}</span>
+    </TD>
+    <TD>
+      <span>
+        {projects?.find((project) => project.id === tx.projectId)?.name}
+      </span>
+    </TD>
+    <TD>
+      <span>${tx.amount.toFixed(2)}</span>
+    </TD>
+    <TD>
+      {path.length > 0 ? (
+        <Button
+          aria-label="Open Invoice Location"
+          type="button"
+          size="sm"
+          variant="ghost"
+          onClick={async () => {
+            await openAttachment(path);
+          }}
+        >
+          {path.startsWith("http://") || path.startsWith("https://") ? (
+            <Globe />
+          ) : (
+            <FolderOpen />
+          )}
+        </Button>
+      ) : (
+        <span className={styles.noFile} aria-label="No attachment">
+          No File
+        </span>
+      )}
+    </TD>
+    <TD>
+      <Button
+        variant="ghost"
+        type="button"
+        onClick={onEdit}
+        aria-label="Edit Transaction"
+      >
+        <Edit />
+      </Button>
+    </TD>
+    <TD>
+      <form
+        onSubmit={async (evt) => {
+          evt.preventDefault();
+          const confirmed = window.confirm(
+            "Are you sure you want to delete this transaction?",
+          );
+          if (!confirmed) return;
+          const fd = new FormData(evt.currentTarget);
+          await onDelete(fd);
+        }}
+      >
+        <input type="hidden" name="id" value={tx.id} />
+        <Button
+          variant="ghost"
+          size="icon"
+          type="submit"
+          aria-label="Delete Transaction"
+        >
+          <TrashIcon />
+        </Button>
+      </form>
+    </TD>
+  </>
 );
